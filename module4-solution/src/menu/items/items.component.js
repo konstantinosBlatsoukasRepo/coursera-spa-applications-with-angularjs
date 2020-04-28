30975a951c0b39b31f14557579b4383d94f4fc64
unknown <konstantinosblatsoukasrepo@gmail.com>
2020-04-28 15:05:22 +0300

    temp commit

--- module4-solution/src/menu/items/items.component.js
+++ module4-solution/src/menu/items/items.component.js
@@ -4,6 +4,10 @@
     angular.module('MenuApp')
         .component('itemsComponent', {
             templateUrl: 'src/menu/items/templates/items.template.html',
+            controller: function () {
+              var menu_items = this.items.menu_items;
+              console.log(menu_items);
+            },
             bindings: {
                 items: '<'
             }
